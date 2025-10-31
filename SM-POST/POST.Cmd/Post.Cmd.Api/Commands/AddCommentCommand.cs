f2d4eb7f41518ed9e89d878374981fde14fbe35e
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-10-31 22:21:12 +0700

    ch03-2. Commands

---
+++ SM-POST/POST.Cmd/Post.Cmd.Api/Commands/AddCommentCommand.cs
@@ -0,0 +1,9 @@
+using CQRS.Core.Commands;
+
+namespace Post.Cmd.Api.Commands;
+
+public class AddCommentCommand : BaseCommand
+{
+    public required string Comment { get; set; }
+    public required string Username { get; set; }
+}
---
+++ SM-POST/POST.Cmd/Post.Cmd.Api/Commands/EditCommentCommand.cs
@@ -0,0 +1,10 @@
+using CQRS.Core.Commands;
+
+namespace Post.Cmd.Api.Commands;
+
+public class EditCommentCommand : BaseCommand
+{
+    public Guid CommentId { get; set; }
+    public required string Comment { get; set; }
+    public required string Username { get; set; }
+}
