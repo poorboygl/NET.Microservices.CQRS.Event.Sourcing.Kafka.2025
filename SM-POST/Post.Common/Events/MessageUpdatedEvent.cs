fcef913826edc696564bb2f57ee7c2d5b90389ea
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-10-31 22:39:10 +0700

    ch03-4. Events

---
+++ SM-POST/Post.Common/Events/MessageUpdatedEvent.cs
@@ -0,0 +1,13 @@
+using System;
+using CQRS.Core.Events;
+
+namespace Post.Common.Events;
+
+public class MessageUpdatedEvent : BaseEvent
+{
+    public MessageUpdatedEvent() : base(nameof(MessageUpdatedEvent))
+    {
+    }
+    
+    public required string Message { get; set; }
+}
