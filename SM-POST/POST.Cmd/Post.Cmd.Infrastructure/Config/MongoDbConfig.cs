7167e61f86f530033d3b09c7b3d1530957ac6753
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-11-01 22:29:35 +0700

    ch06-4. EventStoreRepository

---
+++ SM-POST/POST.Cmd/Post.Cmd.Infrastructure/Config/MongoDbConfig.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Post.Cmd.Infrastructure.Config;
+
+public class MongoDbConfig
+{
+    public required string ConnectionString { get; set; }
+    public required string Database { get; set; }
+    public required string Collection { get; set; }
+}
