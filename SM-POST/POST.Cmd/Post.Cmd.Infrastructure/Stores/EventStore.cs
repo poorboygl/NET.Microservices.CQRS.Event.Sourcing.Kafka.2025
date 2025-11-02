a432e6e714439a973f3abdd21efcfaa6b0251421
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-11-02 15:13:53 +0700

    ch08-3. Producing an Event

--- SM-POST/POST.Cmd/Post.Cmd.Infrastructure/Stores/EventStore.cs
+++ SM-POST/POST.Cmd/Post.Cmd.Infrastructure/Stores/EventStore.cs
@@ -2,11 +2,12 @@ using CQRS.Core.Domain;
 using CQRS.Core.Events;
 using CQRS.Core.Exceptions;
 using CQRS.Core.Infrastructure;
+using CQRS.Core.Producers;
 using Post.Cmd.Domain.Aggregates;
 
 namespace Post.Cmd.Infrastructure.Stores;
 
-public class EventStore(IEventStoreRepository eventStoreRepository) : IEventStore
+public class EventStore(IEventStoreRepository eventStoreRepository, IEventProducer eventProducer) : IEventStore
 {
     public async Task<List<BaseEvent>> GetEventsAsync(Guid aggregateId)
     {
@@ -48,6 +49,9 @@ public class EventStore(IEventStoreRepository eventStoreRepository) : IEventStor
             };
 
             await eventStoreRepository.SaveAsync(eventModel);
+
+            var topic = Environment.GetEnvironmentVariable("KAFKA_TOPIC");
+            await eventProducer.ProduceAsync(topic, @event);
         }
     }
 }
