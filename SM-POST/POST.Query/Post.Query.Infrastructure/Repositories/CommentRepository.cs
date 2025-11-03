75697bfec6e9ae008155ec50bb0ed96b20b0009f
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-11-03 22:40:25 +0700

    ch10-5. CommentRepository

---
+++ SM-POST/POST.Query/Post.Query.Infrastructure/Repositories/CommentRepository.cs
@@ -0,0 +1,43 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Post.Query.Domain.Entities;
+using Post.Query.Domain.Repositories;
+using Post.Query.Infrastructure.DataAccess;
+
+namespace Post.Query.Infrastructure.Repositories;
+
+public class CommentRepository(DatabaseContextFactory contextFactory) : ICommentRepository
+{
+    public async Task CreateAsync(CommentEntity comment)
+    {
+        using DatabaseContext context = contextFactory.CreateDbContext();
+        context.Comments.Add(comment);
+
+        _ = await context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Guid commentId)
+    {
+        using DatabaseContext context = contextFactory.CreateDbContext();
+        var comment = await GetByIdAsync(commentId);
+
+        if (comment == null) return;
+
+        context.Comments.Remove(comment);
+        _ = await context.SaveChangesAsync();
+    }
+
+    public async Task<CommentEntity> GetByIdAsync(Guid commentId)
+    {
+        using DatabaseContext context = contextFactory.CreateDbContext();
+        return await context.Comments.FirstOrDefaultAsync(x => x.CommentId == commentId);
+    }
+
+    public async Task UpdateAsync(CommentEntity comment)
+    {
+        using DatabaseContext context = contextFactory.CreateDbContext();
+        context.Comments.Update(comment);
+
+        _ = await context.SaveChangesAsync();
+    }
+}
