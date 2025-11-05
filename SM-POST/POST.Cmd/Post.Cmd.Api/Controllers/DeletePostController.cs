2c687df4ae6e0ff1675757a621e5dc14c25ba8b7
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-11-05 16:36:27 +0700

    ch13-15. DeletePostController

---
+++ SM-POST/POST.Cmd/Post.Cmd.Api/Controllers/DeletePostController.cs
@@ -0,0 +1,56 @@
+using CQRS.Core.Exceptions;
+using CQRS.Core.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Post.Cmd.Api.Commands;
+using Post.Common.DTOs;
+
+namespace Post.Cmd.Api.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class DeletePostController(ILogger<NewPostController> logger, ICommandDispatcher commandDispatcher) : ControllerBase
+{
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeletePostAsync(Guid id, DeletePostCommand command)
+    {
+        command.Id = id;
+        try
+        {
+
+            await commandDispatcher.SendAsync(command);
+            return Ok(new BaseResponse
+            {
+                Message = "Delete post request completed successfully!"
+            });
+        }
+        catch (InvalidOperationException ex)
+        {
+
+            logger.Log(LogLevel.Warning, ex, "Client made a bad request!");
+            return BadRequest(new BaseResponse
+            {
+                Message = ex.Message
+            });
+        }
+        catch (AggregateNotFoundException ex)
+        {
+
+            logger.Log(LogLevel.Warning, ex, "Could not retrieve aggregate, client passed an incorrect post ID targeting the aggregate  ! ");
+            return BadRequest(new BaseResponse
+            {
+                Message = ex.Message
+            });
+        }
+        catch (Exception ex)
+        {
+            const string SAFE_ERROR_MESSAGE = "error while processing request to Delete a post!";
+            logger.Log(LogLevel.Error, ex, SAFE_ERROR_MESSAGE);
+
+            return StatusCode(StatusCodes.Status500InternalServerError, new BaseResponse
+            {
+                Message = SAFE_ERROR_MESSAGE
+            });
+        }
+    }
+}
